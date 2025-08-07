cc1fef75ea40ac8da084cd9d1e67ddea3e0f0b11
Sam Hollings <52575338+SamHollings@users.noreply.github.com>
2025-08-07 09:00:23 +0000

    Added some example code and bit more commentary

--- src/nhs_reusable_code_library/standard_data_validations/nhsNumberValidation.py
+++ src/nhs_reusable_code_library/standard_data_validations/nhsNumberValidation.py
@@ -1,11 +1,38 @@
+import numpy as np
+
 def mod11_check(nhsNumber :str) -> bool:
     """This function checks if the NHS number passes the MOD 11 check.
     Args:
     nhsNumber (str): The NHS number to be checked.
     Returns:
     bool: True if the NHS number passes the MOD 11 check, False otherwise.
+    Examples:
+    >>> mod11_check("8429141456")
+    True
+    >>> mod11_check("8429141457")
+    False
     """
-    return True
+    # here would go the mod11 code, taking the NHS Number as the input, then doing the calculation, returning true if it passes, or false if it does not.abs
+    # e.g. (taken from NHS Codon Python)
+
+    if not isinstance(nhsNumber, str):
+        raise ValueError("Please input a 10 character string (all integers) to validate (input not a string).")
+    if len(nhsNumber) != 10:
+        raise ValueError("Please input a 10 character string (all integers) to validate (input not 10 characters).")
+    # ToDo: another check that all characters all integers
+
+    digits = [int(digit) for digit in nhsNumber]
+    # Apply weighting to first 9 digits
+    weighted_digits = np.dot(np.array(digits[:9]), np.arange(10, 1, -1))
+    # Validity is based on the check digit, which has to be equal to `remainder`
+    remainder = weighted_digits % 11
+    check_digit = 11 - remainder
+    if check_digit == 11:
+        check_digit = 0
+    if check_digit == digits[-1]:
+        return True
+    else:
+        return False
 
 
 def sensitive_legally_restricted(decision_to_admit :str, nhs_number_status_indicator :str, witheld_identity_reason :str) -> bool:
@@ -17,4 +44,8 @@ def sensitive_legally_restricted(decision_to_admit :str, nhs_number_status_indic
     Returns:
     bool: True if the NHS number is sensitive or legally restricted, False otherwise.
     """
+    # here would go the code for determining sensitive/legally restricted, which is a common exclusion criteria in many CDS completeness rules. 
+    # It would take in the decision to admint, the nhs_number status indicator and the witheld identity reason, and output true if these indicate
+    # the data is sensitive / legally restricted.   
+
     return True
