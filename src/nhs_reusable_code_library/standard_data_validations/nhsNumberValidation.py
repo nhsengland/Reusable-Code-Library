e7e29970c73991ceaa8291c3bcf2ec7167243b9d
Sam Hollings <52575338+SamHollings@users.noreply.github.com>
2025-09-24 14:00:59 +0000

    Added the two functions and tests

--- src/nhs_reusable_code_library/standard_data_validations/nhsNumberValidation.py
+++ src/nhs_reusable_code_library/standard_data_validations/nhsNumberValidation.py
@@ -49,3 +49,53 @@ def sensitive_legally_restricted(decision_to_admit :str, nhs_number_status_indic
     # the data is sensitive / legally restricted.   
 
     return True
+
+
+def nhs_number_format_check(nhs_number : str) -> bool:
+    """This function checks if the NHS number is in the correct format - n10 - as described in the NHS Data Model and Dictionary.
+    Args:
+    nhs_number (str): The NHS number to be checked.
+    Returns:
+    bool: True if the NHS number is in the correct format, False otherwise.
+    Examples:
+    >>> nhs_number_format_check("8429141456")
+    True
+    >>> nhs_number_format_check("842914145")
+    False
+    >>> nhs_number_format_check("84291414567")
+    False
+    >>> nhs_number_format_check("84291414A6")
+    False
+    >>> nhs_number_format_check(8429141456)
+    ValueError: Please input a 10 character string (all integers) to validate (input not a string).
+    """
+    if not isinstance(nhs_number, str):
+        raise ValueError("Please input a 10 character string (all integers) to validate (input not a string).")
+    if len(nhs_number) != 10:
+        return False
+    if not nhs_number.isdigit():
+        return False
+    return True
+
+
+def palindromic_nhs_number_check(nhs_number : str) -> bool:
+    """This function checks if the NHS number is in a list of palindromic numbers which though not valid as NHS Numbers, 
+    can pass the validation checks.
+
+    Args:
+    nhs_number (str): The NHS number to be checked.
+    Returns:
+    bool: True if the NHS number is palindromic, False otherwise.
+    Examples:
+    >>> palindromic_nhs_number("2000000002")
+    True
+    >>> palindromic_nhs_number("1234567890")
+    False
+    """
+    disallowed_nhs_numbers = ('0000000000','1111111111','2222222222','3333333333','4444444444','5555555555',
+                          '6666666666','7777777777','8888888888','9999999999','1000000001','2000000002',
+                          '3000000003','4000000004','5000000005','6000000006','7000000007','8000000008','9000000009')
+
+    if not isinstance(nhs_number, str):
+        raise ValueError("Please input a string to validate (input not a string).")
+    return nhs_number in disallowed_nhs_numbers
